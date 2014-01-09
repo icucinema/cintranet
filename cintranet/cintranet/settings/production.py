3944146a8569792fe3c8b197d5bba4208af38501
Luke Granger-Brown <git@lukegb.com>
2014-01-09 23:43:30 +0000

    Use psycopg2

--- cintranet/cintranet/settings/production.py
+++ cintranet/cintranet/settings/production.py
@@ -4,7 +4,7 @@ DEBUG = TEMPLATE_DEBUG = False
 
 SECRET_KEY = get_env_variable("CINTRANET_SECRET_KEY")
 DATABASES['default'] = {
-    'ENGINE': 'django.db.backends.mysql',
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
     'NAME': 'cintranet',
     'USER': get_env_variable('CINTRANET_DB_USER'),
     'PASSWORD': get_env_variable('CINTRANET_DB_PASSWORD'),
