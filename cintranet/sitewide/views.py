4db541a04539f90c6f70479bdcd9dc30fb3644f7
Luke GB <git@lukegb.com>
2014-01-15 22:42:48 +0000

    Update frontpage to use cott

--- cintranet/sitewide/views.py
+++ cintranet/sitewide/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
-import icunion
-
-ICU_PRODUCT_LIST_PAGE = "https://www.imperialcollegeunion.org/shop/club-society-project-products/cinema-products/"
+import cott.models
 
 class IndexView(TemplateView):
     template_name = 'index.html'
@@ -11,51 +9,44 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         # we're looking for:
         # Membership count
-        cinema_club = icunion.public.Club(411)
-
-        products = icunion.public.ProductList(ICU_PRODUCT_LIST_PAGE).get_products()
-        for product in products:
-            if 'All-Nighter' in product.name:
-                product.initial = 200
+        cm = cott.models.Product.objects.filter(currently_available=True)
 
         my_context = {
             'stats': [
                 {
                     'link': 'https://www.imperialcollegeunion.org/activities/a-to-z/411',
                     'title': 'Members',
-                    'value': cinema_club.get_members(),
+                    'value': cm.get(name__startswith='Cinema Membership ').sold,
                     'class': 'primary'
                 }
             ]
         }
+        cm = cm.exclude(name__startswith='Cinema Membership ')
 
         for product in products:
-            try:
-                sold = product.get_sold()
-                initial = product.initial
+            sold = product.sold
+            initial = product.initial
 
-                btn_class = 'info'
+            btn_class = 'info'
 
-                danger_max = initial * 0.2
-                warning_max = initial * 0.4
-                success_max = initial * 0.75
+            danger_max = initial * 0.2
+            warning_max = initial * 0.4
+            success_max = initial * 0.75
 
-                if sold < danger_max:
-                    btn_class = 'danger'
-                elif sold < warning_max:
-                    btn_class = 'warning'
-                elif sold < success_max:
-                    btn_class = 'success'
+            if sold < danger_max:
+                btn_class = 'danger'
+            elif sold < warning_max:
+                btn_class = 'warning'
+            elif sold < success_max:
+                btn_class = 'success'
 
 
-                my_context['stats'].append({
-                    'link': product.url,
-                    'title': '{} sold'.format(product.name),
-                    'value': sold,
-                    'class': btn_class
-                })
-            except icunion.exceptions.UnionResponseException:
-                pass
+            my_context['stats'].append({
+                'link': 'javascript:;',
+                'title': '{} sold'.format(product.name),
+                'value': sold,
+                'class': btn_class
+            })
 
         if 'view' not in kwargs:
             kwargs['view'] = self
