977751142098a48a9b005fc8bd1a18ba452530f6
Luke GB <git@lukegb.com>
2014-01-15 21:35:30 +0000

    Add admin stuff

--- cintranet/cott/admin.py
+++ cintranet/cott/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.SKU)
+admin.site.register(models.Product)
+admin.site.register(models.SKUEntitlement)
+
+class AuthenticationCredentialAdmin(admin.ModelAdmin):
+    list_display = ('auth_slug', )
+admin.site.register(models.AuthenticationCredential, AuthenticationCredentialAdmin)
--- cintranet/cott/models.py
+++ cintranet/cott/models.py
@@ -6,12 +6,18 @@ class AuthenticationCredential(models.Model):
     auth_slug = models.SlugField(null=False, blank=False)
     auth_data = models.TextField(null=False, blank=False)
 
+    def __unicode__(self):
+        return self.auth_slug
+
 class Product(models.Model):
     org_id = models.PositiveSmallIntegerField(null=True, blank=True)
     eactivities_id = models.PositiveSmallIntegerField(null=True, blank=True)
     name = models.CharField(max_length=256, null=False, blank=True)
     sold = models.PositiveSmallIntegerField(null=True, blank=True)
     initial = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    def __unicode__(self):
+        return self.name
 
 class SKU(models.Model):
     product = models.ForeignKey(Product)
@@ -21,7 +27,13 @@ class SKU(models.Model):
     initial = models.PositiveSmallIntegerField(null=True, blank=True)
     dirty = models.BooleanField(null=False, default=False)
 
+    def __unicode__(self):
+        return self.name
+
 class SKUEntitlement(models.Model):
     sku = models.ForeignKey(SKU)
     entitlement = models.ForeignKey(ticketing.models.Entitlement)
     uses_remaining = models.PositiveSmallIntegerField(null=True, blank=True)
+
+    def __unicode__(self):
+        return u"SKU {} to Entitlement {}".format(sku.name, entitlement.name)
