c7431cbd6cf80bf8bb22001fdf980ad9114bbf89
Luke GB <git@lukegb.com>
2014-01-18 19:43:54 +0000

    Tell Django how to determine if the request is SSL or not

--- cintranet/cintranet/settings/production.py
+++ cintranet/cintranet/settings/production.py
@@ -15,3 +15,5 @@ STATIC_ROOT = "/srv/www/vhosts/staff/httpdocs/static"
 MEDIA_ROOT = "/srv/www/vhosts/staff/httpdocs/media"
 AUTH_LDAP_SERVER_URI = "ldap://localhost"
 AUTH_LDAP_START_TLS = False
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
