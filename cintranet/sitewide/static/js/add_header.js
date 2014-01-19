1650ca197bf6cb64288e312a4b9b856c1a27f138
Luke Granger-Brown <git@lukegb.com>
2014-01-19 16:31:38 +0000

    Add remoteheader system

---
+++ cintranet/sitewide/static/js/add_header.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+	var xhr = new XMLHttpRequest();
+	xhr.onload = function() {
+		// add the header markup
+		var headerEl = document.createElement('header');
+		headerEl.innerHTML = this.responseText;
+		var f = document.body.childNodes[0];
+		if (f)
+			document.body.insertBefore(headerEl, f);
+		else
+			document.body.appendChild(f);
+	
+		// add the header CSS
+		var cssEl = document.createElement('link');
+		cssEl.type = 'text/css';
+		cssEl.rel = 'stylesheet';
+		cssEl.href = '//staff.wide.icucinema.co.uk/static/css/remote_header.css';
+		document.head.appendChild(cssEl);
+	};
+	xhr.open("GET", "https://staff.wide.icucinema.co.uk/remoteheader/", true);
+	xhr.send();
+});
