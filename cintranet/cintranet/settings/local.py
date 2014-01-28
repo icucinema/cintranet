35b8cd66c141ee48c3898646b75ed3f2fccaa48a
Luke Granger-Brown <git@lukegb.com>
2014-01-28 16:57:01 +0000

    That should inherit from base

--- cintranet/cintranet/settings/local.py
+++ cintranet/cintranet/settings/local.py
@@ -1,7 +1,6 @@
-from .production import *
+from .base import *
 
 AUTH_LDAP_SERVER_URI = "ldap://su-cinema-ernie.su.ic.ac.uk"
 AUTH_LDAP_START_TLS = False
 STATIC_ROOT = "static"
 MEDIA_ROOT = "media"
-DEBUG = True
