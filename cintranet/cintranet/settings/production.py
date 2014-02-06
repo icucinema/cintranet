36086a2720ecae3a2f66385690555c1f378c8722
Luke Granger-Brown <luke.granger-brown13@imperial.ac.uk>
2014-02-06 13:49:20 +0000

    Start reporting errors to Sentry

--- cintranet/cintranet/settings/production.py
+++ cintranet/cintranet/settings/production.py
@@ -2,6 +2,14 @@ from .base import *
 
 DEBUG = TEMPLATE_DEBUG = False
 
+INSTALLED_APPS += [
+    'raven.contrib.django.raven_compat',
+]
+
+RAVEN_CONFIG = {
+    'dsn': 'https://19725784cb1843b3b2ee739cd59e9f6e:e28045310d044139a5c30e4c72ad28d6@app.getsentry.com/19013',
+}
+
 SECRET_KEY = get_env_variable("CINTRANET_SECRET_KEY")
 SSO_SHARED_SECRET_KEY = get_env_variable("CINTRANET_SSO_KEY")
 
